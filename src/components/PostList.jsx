29320ea2f0864c7020d353a796708e1350bb8a75
bilalahmad460 <baky75460@gmail.com>
2025-05-07 03:08:29 +0500

    Updated

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,26 +1,26 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Post } from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
 import { WelcomMessage } from "./WelcomMessage";
+import { LoadingSpinner } from "./LoadingSpinner";
 
 export const PostList = () => {
   const { postList, addInitialPost } = useContext(PostListData);
-  const handleGetPostsClick = () => {
+  const [feching, setFeching] = useState(false);
+  useEffect(() => {
+    setFeching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
         addInitialPost(data.posts);
+        setFeching(false);
       });
-  };
-
+  }, []);
   return (
     <>
-      {postList.length === 0 && (
-        <WelcomMessage onGetPostsClick={handleGetPostsClick} />
-      )}
-      {postList.map((post) => (
-        <Post key={post.id} post={post} />
-      ))}
+      {feching && <LoadingSpinner />}
+      {!feching && postList.length === 0 && <WelcomMessage />}
+      {!feching && postList.map((post) => <Post key={post.id} post={post} />)}
     </>
   );
 };
--- src/components/WelcomMessage.jsx
+++ src/components/WelcomMessage.jsx
@@ -2,13 +2,6 @@ export const WelcomMessage = ({ onGetPostsClick }) => {
   return (
     <center className="welcome-message">
       <h1>There are no Posts</h1>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={onGetPostsClick}
-      >
-        Get Post from Server
-      </button>
     </center>
   );
 };
